e94c6c179314cc98f36f6dddf4f35a22aaba0393
SeanC0313 <sean-anthony.cano.622@my.csun.edu>
2026-04-27 14:21:42 -0700

    Added leading zero and timer functions

--- script.js
+++ script.js
@@ -4,12 +4,32 @@ const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
+let timer = [0, 0, 0];
+let interval;
+let timerRunning = false;
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
-
+function leadingZero(time) {
+  if (time <= 9) {
+    time = "0" + time;
+  }
+  return time;
+}
 
 // Run a standard minute/second/hundredths timer:
-
+function runTimer() {
+    let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
+    theTimer.innerHTML = currentTime;
+    timer[2]++;
+    if (timer[2] == 100) {
+        timer[1]++;
+        timer[2] = 0;
+    }
+    if (timer[1] == 60) {
+        timer[0]++;
+        timer[1] = 0;
+    }
+}
 
 // Match the text entered with the provided text on the page:
 
