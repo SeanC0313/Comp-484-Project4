7441ac046c2a4e72f7318ff0c18f4d802f58eccb
SeanC0313 <sean-anthony.cano.622@my.csun.edu>
2026-04-27 15:00:22 -0700

    Added Reset Function

--- script.js
+++ script.js
@@ -60,8 +60,18 @@ function start() {
 }
 
 // Reset everything:
+function reset() {
+    clearInterval(interval);
+    interval = null;
+    timer = [0, 0, 0];
+    timerRunning = false;
 
+    testArea.value = "";
+    theTimer.innerHTML = "00:00:00";
+    testWrapper.style.borderColor = "grey";
+}
 
 // Event listeners for keyboard input and the reset button:
 testArea.addEventListener("keypress", start);
 testArea.addEventListener("keyup", spellCheck);
+resetButton.addEventListener("click", reset);
